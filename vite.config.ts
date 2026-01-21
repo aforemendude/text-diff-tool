8cf4aee58f3b4d5cbd6cfb58729b4d74a98c27ab
aforemendude <95993222+aforemendude@users.noreply.github.com>
2026-01-21 00:34:49 -0500

    feat: Configure Vite to output builds to the `docs` directory and set the base path for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/text-diff-tool/',
   plugins: [react()],
   build: {
     outDir: 'docs',
